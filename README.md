cbd77e7bc7bc6ea8f5cae389f849bc3284d451c7
mrjrdnthms <jordan@everyvine.com>
2014-11-17 10:24:31 -0800

    updated readme and added key to model properties

---
+++ README.md
@@ -0,0 +1,61 @@
+# grunt-loopback-angular-addmodeldata
+
+> Extends grunt-loopback-angular. auto-generating Angular $resource services for loopback will include model data for use at runtime on angular client
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-loopback-angular-addmodeldata --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-loopback-angular-addmodeldata');
+```
+
+## The "loopback_angular_addModelData" task
+
+### Overview
+In your project's Gruntfile, add a section named `loopback_angular_addModelData` to the data object passed into `grunt.initConfig()`.
+
+Service.options shown are defaults so you can leave them off if using the same. Should look like
+
+```js
+loopback_angular_addModelData: {
+      services: {
+        options: {
+          modelConfig: 'server/model-config.json',
+          serviceFile: 'client/app/scripts/lb-services.js',
+          modelDir: 'common/models/'
+        }
+      }
+    }
+```
+Register task as part of loopback command
+
+```js
+grunt.registerTask('loopback', [
+    'loopback_angular',
+	  'loopback_angular_addModelData', <-- Add this line
+    'docular'
+  ]);
+```
+
+### Usage Examples
+So what do you get for this? Now when you access loopback resources in angular you can get at the model info stored in the model.json file. As in
+```js
+//Supposing you have exposed a customer model on your loopback api
+function(Customer) {
+	//figure our what properties the customer model has at runtime
+	modelProps = Customer.model.properties;
+}
+```
+
+I built this because I use [angular-formly](https://github.com/nimbly/angular-formly) to automatically generate forms for editing models. By getting access to model properties at runtime and can build out the forms simply based on the fields defined in model.json
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/loopback_angular_addModelData.js
@@ -0,0 +1,70 @@
+/*
+ * grunt-loopback-angular-addmodeldata
+ * https://github.com/mrjrdnthms/loopback-angular-admin
+ *
+ * Copyright (c) 2014 Jordan Thomas
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var path = require('path');
+
+module.exports = function(grunt) {
+
+	// Please see the Grunt documentation for more information regarding task
+	// creation: http://gruntjs.com/creating-tasks
+
+	grunt.registerMultiTask('loopback_angular_addModelData', 'Extends grunt-loopback-angular. Auto-generated Angular $resource services for loopback will include model data for use at runtime on angular clients', function() {
+		// Merge task-specific and/or target-specific options with these defaults.
+		var options = this.options({
+			modelConfig: 'server/model-config.json',
+			modelDir: 'common/models',
+			serviceFile: 'client/app/scripts/lb-services.js'
+		});
+
+		var serviceFile;
+		grunt.log.ok(options.serviceFile);
+		try {
+			serviceFile = grunt.file.read(options.serviceFile);
+			grunt.log.ok('Loaded LoopBack service file %j', options.serviceFile);
+		} catch (e) {
+			var errService = new Error('Cannot load LoopBack service file ' + options.serviceFile);
+			errService.origError = e;
+			grunt.fail.warn(errService);
+		}
+
+		var models;
+		try {
+			models = require(path.resolve(options.modelConfig));
+			grunt.log.ok('Loaded LoopBack models file %j', options.modelConfig);
+		} catch (e) {
+			var err = new Error('Cannot load LoopBack models file ' + options.modelConfig);
+			err.origError = e;
+			grunt.fail.warn(err);
+		}
+		for (var modelName in models) {
+			if (models[modelName].public){
+				try {
+					var model = require(path.resolve(options.modelDir+modelName+".json"));
+					//grunt.log.ok('Loaded LoopBack model def file %j', modelName);
+				} catch (e) {
+					var modelErr = new Error('Cannot load LoopBack model file for ' + modelName);
+					modelErr.origError = e;
+					grunt.fail.warn(modelErr);
+				}
+				for (var prop in model.properties) {
+					model.properties[prop].key = prop;
+				}
+				var regex = new RegExp("(module\\.factory\\([\\s]*\""+modelName+"\"[\\s\\S]*?)(return R;)");
+				var replace = '$1'+'R.model='+JSON.stringify(model)+';\n\n'+'$2';
+				serviceFile = serviceFile.replace(regex,replace);
+			}
+		}
+
+		//grunt.log.ok(serviceFile);
+		grunt.file.write(options.serviceFile,serviceFile);
+		grunt.log.ok('Modified angular services to include a new property "model" which holds the model json object', options.output);
+	});
+
+};
